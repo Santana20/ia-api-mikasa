a751a8ee08bbfb3eae925afc9b4bd97c32a1d2cf
Santana20 <sstfmscontreras@gmail.com>
2022-06-11 23:50:12 -0500

    11vo intento deploy

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+application = Flask(__name__)
 # Configuracion
-app.secret_key = '123456'
+application.secret_key = '123456'
 
 
-@app.route("/", methods=['GET'])
+@application.route("/", methods=['GET'])
 def hello_world():
     return jsonify({'mensaje': 'te comunicas con el API python.'})
 
@@ -13,5 +13,5 @@ def endpoint_no_encontrado(error):
   return jsonify({'error': 'error', 'mensaje': 'el endpoint no existe.'})
 
 if __name__ == '__main__':
-  app.register_error_handler(404, endpoint_no_encontrado)
-  app.run()
+  application.register_error_handler(404, endpoint_no_encontrado)
+  application.run()
