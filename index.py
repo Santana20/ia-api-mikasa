90aff6b4b61acdcbc8d4b337af54c09425327ddb
Santana20 <sstfmscontreras@gmail.com>
2022-06-18 11:57:47 -0500

    agregado validacion de cmp de usuaria

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import random
 
 application = Flask(__name__)
 # Configuracion
@@ -7,11 +8,30 @@ application.secret_key = '123456'
 
 @application.route("/", methods=['GET'])
 def hello_world():
-    return jsonify({'mensaje': 'te comunicas con el API python.'})
+  return jsonify({'mensaje': 'te comunicas con el API python.'})
+
+@application.route("/cmp", methods=['POST'])
+def validar_cmp():
+  if request.method == "POST":
+    success = False
+    mensaje = 'No se encontro usuario registrado con el cmp: {0}'
+    
+    cmp = request.json['cmp']
+    
+    # simulamos validacion de cmp
+    num = random.random()
+    if num >= 0.5:
+      success = True
+      mensaje = "Se encontro usuario registrado con el cmp: {0}"
+    
+    return jsonify({'success': success, 'mensaje': mensaje.format(cmp)})
+
+
+
 
 def endpoint_no_encontrado(error):
   return jsonify({'error': 'error', 'mensaje': 'el endpoint no existe.'})
 
 application.register_error_handler(404, endpoint_no_encontrado)
 if __name__ == '__main__':
-  application.run()
+  application.run(debug=True)
